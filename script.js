277b2a6063f2500a3b72cac003ba722bde1ebb80
ArpanSankesh <arpansankesh830@gmail.com>
2024-10-27 01:49:28 +0530

    Context Api

--- script.js
+++ script.js
@@ -1,7 +1,17 @@
-gsap.to('#box', {
-    x:500, 
-    duration:1.5, 
-    delay:1, 
-    borderRadius:"50%", 
-    rotate:360
+gsap.from('.page1 #box', {
+    scale:0,
+    duration:2.5,
+    rotate:360,
+})
+
+gsap.from('.page2 #box', {
+    scale:0,
+    duration:2.5,
+    rotate:360,
+    scrollTrigger : {
+        trigger: '.page2 #box',
+        scroller: 'body',
+        markers:true,
+        scrub:1
+    }
 })
