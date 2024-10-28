8eaa7e8234e1bd34d9f4131f6bb1a75715ca3493
ArpanSankesh <arpansankesh830@gmail.com>
2024-10-28 23:09:08 +0530

    Context Api

--- script.js
+++ script.js
@@ -1,17 +1,7 @@
-gsap.from('.page1 #box', {
-    scale:0,
-    duration:2.5,
-    rotate:360,
-})
+var main = document.querySelector(".main")
 
-gsap.from('.page2 #box', {
-    scale:0,
-    duration:2.5,
-    rotate:360,
-    scrollTrigger : {
-        trigger: '.page2 #box',
-        scroller: 'body',
-        markers:true,
-        scrub:1
-    }
+var scroll = new LocomotiveScroll({
+    el:main,
+    smooth:true,
+    lerp:0.05
 })
