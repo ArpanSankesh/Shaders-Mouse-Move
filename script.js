6c95cc84f124012a2709e86f49cd114c346b35f4
ArpanSankesh <arpansankesh830@gmail.com>
2024-10-31 12:34:08 +0530

    Context Api

--- script.js
+++ script.js
@@ -1,15 +1,6 @@
-// Initialize Lenis
-const lenis = new Lenis();
-
-// Listen for the scroll event and log the event data
-lenis.on('scroll', (e) => {
-  console.log(e);
-});
-
-// Use requestAnimationFrame to continuously update the scroll
-function raf(time) {
-  lenis.raf(time);
-  requestAnimationFrame(raf);
-}
-
-requestAnimationFrame(raf);
+var swiper = new Swiper('.swiper',{
+    navigation:{
+        nextEl:'.next',
+        prevEl:'.prev'
+    }
+})
