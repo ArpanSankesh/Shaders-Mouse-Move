9684443c42b6c0c58f749734f971a40781bc9775
ArpanSankesh <arpansankesh830@gmail.com>
2024-10-31 12:51:36 +0530

    Context Api

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 var swiper = new Swiper('.swiper',{
-    navigation:{
-        nextEl:'.next',
-        prevEl:'.prev'
-    }
+    pagination:{
+       el:'.swiper-pagination',
+       dynamicBullets:true
+    },
+    loop:true,
+    autoplay:true
 })
